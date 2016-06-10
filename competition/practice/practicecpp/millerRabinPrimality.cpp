c626c406d368a7c3bdc645ee8674457f0144142a
Arulkumar <arul.subramaniam@hotmail.com>
2016-06-11 01:26:34 +0530

    primality check (miller rabin) algorithm is added

--- competition/practice/practicecpp/millerRabinPrimality.cpp
+++ competition/practice/practicecpp/millerRabinPrimality.cpp
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Arulkumar (arul.csecit@ymail.com).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Arulkumar (arul.csecit@ymail.com)
+ *     https://www.hackerearth.com/practice/math-1/number-theory-1/primality-tests-1/tutorial/
+ *******************************************************************************/
 #include <stdio.h>
 #include <iostream>
 #include <stdlib.h>
