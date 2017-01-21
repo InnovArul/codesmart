6128e44f9bf1a3a7916032ff1b477b536c140d22
Arulkumar <arul.subramaniam@hotmail.com>
2017-01-21 22:43:02 +0530

    assignment-2 files for image processing are added

--- Assignments/Jan-May 2017/DigitalImageProcessing/utils.py
+++ Assignments/Jan-May 2017/DigitalImageProcessing/utils.py
@@ -91,32 +91,3 @@ def translateImage(img, tx, ty):
             translatedImage[i, j] = bilinearInterpolate(img, iSourceX, iSourceY)
 
     return translatedImage
-
-def main(imgpath, name):
-    # load the image in grey scale
-    img = cv2.imread(imgpath, 0);
-
-
-    # translate the image
-    translatedImage = translateImage(img, 3.75, 4.3)
-
-    # rotate the image
-    rotatedImage = rotateImage(img, 3)
-
-    # scale the image
-    scaledImage = scaleImage(img, 0.8, 1.3)
-
-    cv2.imwrite('./Lab1/tr' + name + '.png', translatedImage)
-    cv2.imwrite('./Lab1/rot' + name + '.png', rotatedImage)
-    cv2.imwrite('./Lab1/sc' + name + '.png', scaledImage)
-
-    # cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-    # cv2.imshow('image', rotatedImage)
-    # cv2.imshow('image', translatedImage)
-    # cv2.imshow('image', scaledImage)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-
-main("./Lab1/pisa.pgm", 'pisa')
-# main("./Lab1/lena.pgm", 'lena')
-# main("./Lab1/cells.pgm", 'cells')
