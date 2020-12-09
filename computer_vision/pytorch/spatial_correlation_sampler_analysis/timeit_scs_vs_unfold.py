5a4b3e02fbc2668ea276d3526017a1b066dec91b
Arul <arul.csecit@ymail.com>
2020-12-09 23:25:58 +0530

    comparing spatial_correlation_sampler vs unfold

---
+++ computer_vision/pytorch/spatial_correlation_sampler_analysis/timeit_scs_vs_unfold.py
@@ -0,0 +1,48 @@
+import os, sys
+import spatial_correlation_sampler as scs
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import argparse, time
+from tqdm import tqdm
+
+def scs_correlation(input1, input2, P, K=1, dilation_K=1, dilation_P=1):
+    out = scs.spatial_correlation_sample(input1, input2, patch_size=P,)
+    return out
+
+def unfold_correlation(input1, input2, K=1, dilation=1):
+      batch, c, h, w = input2.shape
+      temp = F.unfold(input2, kernel_size=3, padding=1)
+      temp = temp.view(batch,c,3,3,h,w)
+      input1 = input1.view(batch, c, 1, 1, h, w)
+      out = (input1 * temp).sum(dim=1)
+      return out
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="SCS analysis")
+    parser.add_argument("--height", type=int, default=25)
+    parser.add_argument("--width", type=int, default=35)
+    parser.add_argument("--channels", type=int, default=128)
+    parser.add_argument("--batch", type=int, default=10)
+    parser.add_argument("--patch", type=int, default=3)
+    parser.add_argument("--times", type=int, default=100)
+
+    args = parser.parse_args()
+
+    input1 = torch.randn(args.batch, args.channels, args.height, args.width).cuda()
+    input2 = torch.randn(args.batch, args.channels, args.height, args.width).cuda()
+
+    start = time.time()
+    for i in tqdm(range(args.times)):
+        scs_out = scs_correlation(input1, input2, P=args.patch)
+    end = time.time()
+    print("scs", (end-start) / args.times)
+
+    start = time.time()
+    for i in tqdm(range(args.times)):
+        unfold_out = unfold_correlation(input1, input2, K=args.patch)
+    end = time.time()
+    print("unfold", (end-start) / args.times)
+
+    #diff = ((scs_out - unfold_out)**2).sum()
+    #print("error", diff, scs_out[:,:,:,7,7], unfold_out[:,:,:,7,7])
