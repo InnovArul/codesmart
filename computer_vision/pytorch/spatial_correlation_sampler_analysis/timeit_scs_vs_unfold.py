6570636792abdd90e2219c823e8e89b6a06bf97b
Arul <arul.csecit@ymail.com>
2020-12-12 09:38:17 +0530

    dilation added

--- computer_vision/pytorch/spatial_correlation_sampler_analysis/timeit_scs_vs_unfold.py
+++ computer_vision/pytorch/spatial_correlation_sampler_analysis/timeit_scs_vs_unfold.py
@@ -12,8 +12,8 @@ def scs_correlation(input1, input2, P, K=1, dilation_K=1, dilation_P=1):
 
 def unfold_correlation(input1, input2, K=1, dilation=1):
       batch, c, h, w = input2.shape
-      temp = F.unfold(input2, kernel_size=3, padding=1)
-      temp = temp.view(batch,c,3,3,h,w)
+      temp = F.unfold(input2, kernel_size=K, padding=K//2, dilation=dilation)
+      temp = temp.view(batch,c,K,K,h,w)
       input1 = input1.view(batch, c, 1, 1, h, w)
       out = (input1 * temp).sum(dim=1)
       return out
@@ -26,21 +26,27 @@ if __name__ == "__main__":
     parser.add_argument("--batch", type=int, default=10)
     parser.add_argument("--patch", type=int, default=3)
     parser.add_argument("--times", type=int, default=100)
+    parser.add_argument("--dilation", type=int, default=1)
 
     args = parser.parse_args()
 
     input1 = torch.randn(args.batch, args.channels, args.height, args.width).cuda()
     input2 = torch.randn(args.batch, args.channels, args.height, args.width).cuda()
 
+    scs_out = scs_correlation(input1, input2, P=args.patch, dilation_P=args.dilation)
+    unfold_out = unfold_correlation(input1, input2, K=args.patch, dilation=args.dilation)
+    print("diff", torch.sum((scs_out - unfold_out)**2))
+    print(scs_out[0,:,:,5,2], "\n", unfold_out[0,:,:,5,2])
+
     start = time.time()
     for i in tqdm(range(args.times)):
-        scs_out = scs_correlation(input1, input2, P=args.patch)
+        scs_out = scs_correlation(input1, input2, P=args.patch, dilation_P=args.dilation)
     end = time.time()
     print("scs", (end-start) / args.times)
 
     start = time.time()
     for i in tqdm(range(args.times)):
-        unfold_out = unfold_correlation(input1, input2, K=args.patch)
+        unfold_out = unfold_correlation(input1, input2, K=args.patch, dilation=args.dilation)
     end = time.time()
     print("unfold", (end-start) / args.times)
 
